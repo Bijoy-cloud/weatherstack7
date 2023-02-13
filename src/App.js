783f48dab13c8994b46322759a583cf794aa0740
Bijoy-cloud <bishalbijoy98@gmail.com>
2023-02-13 19:19:25 +0530

    api catch error  added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import ReactDOM  from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/HomeScreen/Home";
+import "./App.css";
 
-import Home from './components/HomeScreen/Home'
-import './App.css'
-
-if (process.env.NODE_ENV !== 'production') {
-    console.log('Looks like we are in development mode!');
-  }
+if (process.env.NODE_ENV !== "production") {
+  console.log("Looks like we are in development mode!");
+}
 export default function App() {
-    
-    return (
-        <div className='app-container' >
-       <Home />
-      
-        </div>
-    )
+  return (
+    <div className="app-container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-ReactDOM.render(<App/>,document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"));
